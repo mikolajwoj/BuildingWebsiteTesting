bf78ab63b4e9cf9bb51ed1d37038a07d951897f8
MikolajWojcik <mikolaj.wojcik@doverfs.com>
2022-09-20 22:31:24 +0200

    Delete unused dependencies in one page.

--- BuildingWebsiteTestProject/Pages/SkyScreeperPage.cs
+++ BuildingWebsiteTestProject/Pages/SkyScreeperPage.cs
@@ -1,12 +1,4 @@
-using System;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Remote;
-using NUnit.Framework;
-using System.Threading;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BuildingWebsiteTestProject.Base;
 using OpenQA.Selenium.Support.UI;
 // For supporting Page Object Model
